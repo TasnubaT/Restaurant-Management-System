12f6641842408828592fb5aabbc5fa784c62e82b
TasnubaT <flower.32.floweru@gmail.com>
2024-07-18 10:41:44 +0600

    Firebase Config Initialize

---
+++ rms-client/src/Auth/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmhWGCSoTdik8-7PgamJxNcAnNgM8BtD8",
+  authDomain: "restaurant-ms-4dcac.firebaseapp.com",
+  projectId: "restaurant-ms-4dcac",
+  storageBucket: "restaurant-ms-4dcac.appspot.com",
+  messagingSenderId: "591915983618",
+  appId: "1:591915983618:web:bc9fc5fcbed2d16220581b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
