709d57ae40140804e49084d4425064cdeacd60ac
TasnubaT <flower.32.floweru@gmail.com>
2024-08-19 16:57:04 +0600

    middlewares for verification

--- rms-server/index.js
+++ rms-server/index.js
@@ -32,6 +32,49 @@ app.post("/jwt", async (req, res) => {
   res.send({ token });
 });
 
+// ----------------------
+
+// Middlewares
+// Verify Token
+const verifyToken = (req, res, next) => {
+  console.log("inside verify", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// Verify Admin
+const verifyAdmin = (req, res, next) => {
+  const email = req.decoded.email;
+
+  const sql = "SELECT userRole FROM users WHERE userEmail = ?";
+
+  db.query(sql, [email], (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    const isAdmin =
+      results && results.length > 0 && results[0].userRole === "admin";
+
+    if (!isAdmin) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+
+    next();
+  });
+};
+
+const queryAsync = util.promisify(db.query);
 
 
 
